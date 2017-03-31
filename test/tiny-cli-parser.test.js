c7b5c1c3b3a5b41d418bda509e95d5da3cb83747
jason child <jason@unearthlabs.com>
2017-03-30 18:37:10 -0700

    init

---
+++ test/tiny-cli-parser.test.js
@@ -0,0 +1,6 @@
+const parser = require('../src/tiny-cli-parser').tinyCliParser
+const validate = require('../src/tiny-cli-parser').validate
+
+describe('tiny-cli-parser', () => {
+  it('is cool', () => true)
+})
